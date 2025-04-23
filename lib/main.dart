0f5edea9297aa9c29f65cc3fb5a46aca69a1d560
leehyunjin <skyhyunjinlee@gmail.com>
2025-04-23 21:50:15 +0900

    feat : 디바이스 세로 방향만 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 import 'package:with_run_app/ui/pages/map/map_page.dart';
+import 'package:with_run_app/ui/pages/login/login_page.dart';
 
-void main() {
+void main() async {
+  // main함수에 async 사용할 때, 시스템 설정을 위해
+  // runApp보다 먼저 호출
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 세로 방향만 허용
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -12,7 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MapPage(),
+      theme: ThemeData(highlightColor: Color(0xff2196F3)),
+      home: LoginPage(),
     );
   }
 }
