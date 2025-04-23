889b56c0d209eed665be152c5167bdc87b25c07e
leehyunjin <skyhyunjinlee@gmail.com>
2025-04-23 23:24:35 +0900

    feat : firebase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
-import 'package:with_run_app/ui/pages/map/map_page.dart';
+import 'package:with_run_app/firebase_options.dart';
 import 'package:with_run_app/ui/pages/login/login_page.dart';
 
 void main() async {
-  // main함수에 async 사용할 때, 시스템 설정을 위해
-  // runApp보다 먼저 호출
+  // main함수에 async awati을 사용해 비동기 함수를
+  // 호출하면, 시스템 설정을 위해 runApp보다 먼저 호출
   WidgetsFlutterBinding.ensureInitialized();
 
   // 세로 방향만 허용
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  // firebase 초기화
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(ProviderScope(child: MyApp()));
 }
