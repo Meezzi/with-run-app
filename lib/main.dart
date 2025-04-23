a91dfaddbe44c75db14eb268e819ad1caf18afe0
Meezzi <alswl-3016@naver.com>
2025-04-23 20:23:37 +0900

    feat: 시작 화면을 MapPage로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:with_run_app/ui/pages/home/home_page.dart';
+import 'package:with_run_app/ui/pages/map/map_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(useMaterial3: true),
+      home: MapPage(),
     );
   }
 }
