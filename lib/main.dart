bb3874070dad99f2e73815fc3b8d082ddfa0ae78
Meezzi <alswl-3016@naver.com>
2025-04-23 16:42:56 +0900

    feat: home을 HomePage로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:with_run_app/ui/pages/home/home_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
