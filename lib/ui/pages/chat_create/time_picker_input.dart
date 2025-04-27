6d9afccdf5a0c1d8eee403c0534154a95edf4a9b
arcmee <kihyun3297@gmail.com>
2025-04-27 22:24:46 +0900

    feat : 혹시몰라 timepicker 까지지

---
+++ lib/ui/pages/chat_create/time_picker_input.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class TimePickerInput extends StatefulWidget {
+  const TimePickerInput({super.key});
+
+  @override
+  State<TimePickerInput> createState() => _TimePickerInputState();
+}
+
+class _TimePickerInputState extends State<TimePickerInput> {
+  TimeOfDay? selectedTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          selectedTime == null ? '시간을 설정해주세요' : '${selectedTime!.hour} : ${selectedTime!.minute}',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(width: 10,),
+        ElevatedButton(
+          child: const Text('시간 설정'),
+          onPressed: () async {
+            final TimeOfDay? time = await showTimePicker(
+              context: context,
+              initialTime: selectedTime ?? TimeOfDay.now(),
+              initialEntryMode: TimePickerEntryMode.inputOnly,
+              orientation: Orientation.landscape,
+              builder: (BuildContext context, Widget? child) {
+                return Theme(
+                  data: Theme.of(
+                    context,
+                  ).copyWith(materialTapTargetSize: MaterialTapTargetSize.shrinkWrap),
+                  child: Directionality(
+                    textDirection: TextDirection.ltr,
+                    child: MediaQuery(
+                      data: MediaQuery.of(
+                        context,
+                      ).copyWith(alwaysUse24HourFormat: false),
+                      child: child!,
+                    ),
+                  ),
+                );
+              },
+            );
+            setState(() {
+              selectedTime = time;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
