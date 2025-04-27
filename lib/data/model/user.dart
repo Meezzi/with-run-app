7f0125e1b6f547f39d59e6b296aa07d910441de9
arcmee <kihyun3297@gmail.com>
2025-04-27 20:21:35 +0900

    feat : datePicker 추가가

---
+++ lib/data/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String nickname;
+  String name;
+  String email;
+  String profileImageUrl;
+  String id;
+
+  User({
+    required this.id,
+    required this.nickname,
+    required this.name,
+    required this.email,
+    required this.profileImageUrl,
+  });
+}
