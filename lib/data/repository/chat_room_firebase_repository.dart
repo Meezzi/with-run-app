6708257787ec0c69a4d10fc7947469a4799c520b
arcmee <kihyun3297@gmail.com>
2025-04-29 03:53:04 +0900

    fix : 채팅 방 생성 시 방 id가져오도록 수정, 방정보 받아오는 곳 예외처리

--- lib/data/repository/chat_room_firebase_repository.dart
+++ lib/data/repository/chat_room_firebase_repository.dart
@@ -1,28 +1,45 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:with_run_app/data/model/chat_room_model.dart';
 import 'package:with_run_app/data/model/user.dart';
 import 'package:with_run_app/data/repository/chat_room_repository.dart';
 
-class ChatRoomFirebaseRepository implements ChatRoomRepository{
-
+class ChatRoomFirebaseRepository implements ChatRoomRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
-  Future<void> create(ChatRoomModel chatRoom, User creator) async{
-    await _firestore.collection('chatRooms').add(chatRoom.toMap()).then((documentSnapshot){
-      print("Added Data with ID: ${documentSnapshot.id}");
-      documentSnapshot.collection('participants').doc(creator.uid).set({
-        'nickName' : creator.nickname,
-        'profileImageUrl' : creator.profileImageUrl,
-      });
-    });
-  }
-  
-  @override
-  Future<ChatRoomModel> get(String id) async{
-    final result = await _firestore.collection('chatRooms').doc(id).get();
-    return ChatRoomModel.fromFirestore(result);
+  Future<String> create(ChatRoomModel chatRoom, User creator) async {
+    final result = await _firestore
+        .collection('chatRooms')
+        .add(chatRoom.toMap())
+        .then((documentSnapshot) {
+          print("Added Data with ID: ${documentSnapshot.id}");
+          addParticipant(creator, documentSnapshot.id);
+          return documentSnapshot.id;
+        });
+    return result;
   }
 
+  @override
+  Future<ChatRoomModel> get(String id) async {
+    final doc = await _firestore.collection('chatRooms').doc(id).get();
+
+    if (!doc.exists) {
+      throw Exception('ChatRoom not found');
+    }
+
+    return ChatRoomModel.fromFirestore(doc);
+  }
+
+  @override
+  Future<void> addParticipant(User user, String chatRoomId) async {
+    _firestore
+        .collection('chatRooms')
+        .doc(chatRoomId)
+        .collection('participants')
+        .doc(user.uid)
+        .set({
+          'nickName': user.nickname,
+          'profileImageUrl': user.profileImageUrl,
+        });
+  }
 }
--- lib/data/repository/chat_room_repository.dart
+++ lib/data/repository/chat_room_repository.dart
@@ -4,7 +4,9 @@ import 'package:with_run_app/data/model/chat_room_model.dart';
 import 'package:with_run_app/data/model/user.dart';
 
 abstract interface class ChatRoomRepository {
-  Future<void> create(ChatRoomModel chatRoom, User creator);
+  Future<String> create(ChatRoomModel chatRoom, User creator);
 
-  Future<void> get(String id);
+  Future<ChatRoomModel> get(String id);
+
+  Future<void> addParticipant(User user, String chatRoomId);
 }
