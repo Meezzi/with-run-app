e20549fd3de069c84e5483abe2835d45ec86ae49
Meezzi <alswl-3016@naver.com>
2025-04-28 20:55:12 +0900

    feat : RunningViewModel 생성

--- lib/ui/pages/rank/running_view_model.dart
+++ lib/ui/pages/rank/running_view_model.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:with_run_app/data/repository/running_repository.dart';
+
 // RunningState 클래스: 러닝의 상태를 저장
 class RunningState {
   final bool isStart;
@@ -37,4 +42,26 @@ class RunningState {
       errorMessage: errorMessage ?? this.errorMessage,
     );
   }
+}
+
+class RunningViewModel extends StateNotifier<RunningState> {
+  final RunningRepository runningRepository;
+  final Stopwatch _stopwatch = Stopwatch(); // 시간 측정을 위한 Stopwatch
+  StreamSubscription<StepCount>? _stepSubscription; // 걸음 수 스트림
+  int _initialSteps = 0; // 초기 걸음 수
+  int _currentSteps = 0; // 현재 걸음 수
+
+  RunningViewModel({required String chatRoomId})
+    : runningRepository = RunningRepository(chatRoomId: chatRoomId),
+      super(
+        RunningState(
+          isStart: false,
+          steps: '0',
+          distance: '0.0',
+          speed: '0.0',
+          colories: '0',
+          runningTime: '0',
+          errorMessage: '',
+        ),
+      );
 }
