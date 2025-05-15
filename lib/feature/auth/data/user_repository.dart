7c6339b5dec3935eeef30879470b8b28cb883e18
Meezzi <alswl-3016@naver.com>
2025-05-15 19:32:01 +0900

    refactor : 기능 우선 구조로 인한 auth의 data 하위 폴더로 이동

--- lib/feature/auth/data/user_repository.dart
+++ lib/feature/auth/data/user_repository.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:with_run_app/data/model/user.dart';
+import 'package:with_run_app/feature/auth/data/user.dart';
 
 class UserRepository {
   /// firebase database에 로그인한 User정보 추가
