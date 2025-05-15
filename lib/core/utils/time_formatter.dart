2e1a1d2be1d1caa6f24e051e67dd56db00f0f005
Meezzi <alswl-3016@naver.com>
2025-05-15 19:27:03 +0900

    remove : 사용하지 않는 파일 삭제

--- lib/core/utils/time_formatter.dart
+++
@@ -1,6 +0,0 @@
-String _formatTime(dynamic runningTime) {
-  final seconds = int.tryParse(runningTime?.toString() ?? '0') ?? 0;
-  final minutes = seconds ~/ 60;
-  final secs = seconds % 60;
-  return '${minutes.toString().padLeft(2, '0')}:${secs.toString().padLeft(2, '0')}';
-}
