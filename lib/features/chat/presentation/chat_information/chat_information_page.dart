beb9c7a01a7d9be7849cb3ee84c110e5b3b4260b
leehyunjin <skyhyunjinlee@gmail.com>
2025-05-21 20:21:29 +0900

    refactor: chat_information_page

--- lib/features/chat/presentation/chat_information/chat_information_page.dart
+++ lib/features/chat/presentation/chat_information/chat_information_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:with_run_app/features/chat/data/chat_room_model.dart';
 import 'package:with_run_app/features/chat/presentation/chat_information/widgets/chat_participant_list.dart';
-import 'package:with_run_app/ui/pages/chatting_page/chat_room_view_model.dart';
+import 'package:with_run_app/features/chat/presentation/chat_room/viewmodels/chat_room_view_model.dart';
 
 class ChatInformationPage extends ConsumerWidget {
   const ChatInformationPage({super.key});
-  
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final chatRoom = ref.watch(chatRoomViewModel);
@@ -20,8 +19,11 @@ class ChatInformationPage extends ConsumerWidget {
         height: double.infinity,
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: ChatParticipantList(participants : chatRoom?.participants ?? [], creator: chatRoom!.creator!,),
-        )
+          child: ChatParticipantList(
+            participants: chatRoom?.participants ?? [],
+            creator: chatRoom!.creator!,
+          ),
+        ),
       ),
     );
   }
