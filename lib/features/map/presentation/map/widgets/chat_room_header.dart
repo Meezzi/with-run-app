775992a0f0419ecb9eacb890e430eb7d175b64dc
Meezzi <alswl-3016@naver.com>
2025-06-03 15:21:42 +0900

    feat : 채팅방의 이미지, 제목, 참여 인원을 보여주는 채팅방 헤더 위젯 추가

---
+++ lib/features/map/presentation/map/widgets/chat_room_header.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:with_run_app/features/map/presentation/map/map_view_model.dart';
+
+class ChatRoomHeader extends StatelessWidget {
+  const ChatRoomHeader(this.chatroom, {super.key});
+
+  final ChatRoom chatroom;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.blue[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Icon(Icons.group, color: Colors.blue[600], size: 30),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                chatroom.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  Icon(Icons.people, size: 16, color: Colors.grey[600]),
+                  const SizedBox(width: 4),
+                  Text(
+                    '${chatroom.memberCount}명 참여중',
+                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
